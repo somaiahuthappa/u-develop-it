cb92e9055d3de2194ff34157e49a2de99c11ba9e
Som's Laptop <somaiahuthappa@gmail.com>
2022-03-27 14:41:58 -0400

    linked server to database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,24 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username
+        user: 'root',
+        // MySQL password
+        password: 'Underhill_10@',
+        database: 'election'
+    },
+    console.log('Connected to the electin database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 
 // Default request for any other request (Not Found)
 app.use((req, res) => {
